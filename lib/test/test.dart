824a7411bb743cffc98e8fef2b5baeb2feb3e35c
Yeonho Suh <yeonhosuh1@naver.com>
2023-06-12 18:21:09 +0900

    test init

--- lib/test/test.dart
+++ lib/test/test.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:project_bab/sub/DbGet.dart';
+import 'dart:math';
 
-class UserInfoSave extends StatelessWidget {
-  const UserInfoSave({Key? key}) : super(key: key);
+void createTestUser() async {
+  Random random = Random();
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  final age = (random.nextInt(10)+20).toString();
+  final profileimg = random.nextInt(8)+1;
+  Map<String, dynamic> userData = {
+    "age" : age,
+    "checked" : true,
+    "gender" : "woman",
+    "introduction" : "자기소개입니다",
+    "kakaotalkid" : "kakaolid",
+    "mannertemp" : 36.5,
+    "matched" : false,
+    "matchingUser" : "",
+    "name" : "테스트",
+    "nickname" : "닉네임",
+    "profileimg" : profileimg,
+    "uid" : "uid필요"
+  };
+}
+
+void deleteTestUser() async {
+  ;
 }
